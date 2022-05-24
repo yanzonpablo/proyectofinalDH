80e3702eb52589c6ef15277686c9c4230b797694
nicoquartero <nicoquartero@gmail.com>
2022-05-24 18:50:51 -0300

    Commit con primer version de home completo

---
+++ desarrollo/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+app.listen(3010, () => {
+  console.log("Servidor corriendo en puerto 3010");
+});
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/index.html"));
+});
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/login.html"));
+});
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/register.html"));
+});
+app.get("/nav", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/nav.html"));
+});
