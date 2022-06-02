4d72a620474411bac309f7a7377ed47fb5b94f57
nicoquartero <nicoquartero@gmail.com>
2022-06-02 19:00:03 -0300

    Plantilla dinamica para la seccion de ofertas en Home

--- desarrollo/productos.js
+++ desarrollo/productos.js
@@ -1,5 +1,6 @@
 // Categorias:
 // Notebooks // Televisores // Entretenimiento // Celulares // Impresoras // Heladeras // Cafeteras
+// Tablets
 const productos = [
   {
     nombre: 'Notebook Lenovo de 14" HD',
@@ -100,6 +101,56 @@ const productos = [
     categoria: "Cafeteras",
     destacado: true,
     oferta: false,
+  },
+  {
+    nombre: "Tablet Samsung A8",
+    descripcion:
+      'Tablet de 10.5" TFT WUXGA (1920 x 1200). Procesador Octa Core (CPU speed 2 GHz)',
+    precio: 47500,
+    precioantes: 61750,
+    id: 11,
+    imagen: "tablet-samsung-a8.jpg",
+    categoria: "Tablets",
+    destacado: false,
+    oferta: true,
+    descuento: 30,
+  },
+  {
+    nombre: "Notebook PCBOX PCB",
+    descripcion: 'Notebook - Cloubook. Pantalla 14" FHD (1920x1080 Px)',
+    precio: 38500,
+    precioantes: 48000,
+    id: 12,
+    imagen: "notebook-pcbox-celeron.jpg",
+    categoria: "Notebooks",
+    destacado: false,
+    oferta: true,
+    descuento: 25,
+  },
+  {
+    nombre: "Lavarropas Dream Concept",
+    descripcion: "Panel electronico. CDA (control digital de agitacion)",
+    precio: 45760,
+    precioantes: 35200,
+    id: 13,
+    imagen: "lavarropas-automatico-dream.jpg",
+    categoria: "Lavarropas",
+    destacado: false,
+    oferta: true,
+    descuento: 30,
+  },
+  {
+    nombre: "Lavarropas Dream Concept",
+    descripcion:
+      "La portabilidad de una gran tablet para el entretenimiento y la comodidad",
+    precio: 23475,
+    precioantes: 18780,
+    id: 14,
+    imagen: "tablet-lenovo-8505fs.jpg",
+    categoria: "Tablets",
+    destacado: false,
+    oferta: true,
+    descuento: 25,
   },
 ];
 
