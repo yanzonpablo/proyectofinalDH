2e6da68205de12da283989e1dfe620427a30c67e
EnriqueAlfonso <enrique.alfonso.lema@gmail.com>
2022-06-06 18:58:34 -0300

    06/06/2022

--- desarrollo/app.js
+++ desarrollo/app.js
@@ -1,25 +1,27 @@
+const fs = require("fs");
 const express = require("express");
 const path = require("path");
 const app = express();
+
 app.listen(3010, () => {
-  console.log("Servidor corriendo en puerto 3010");
+    console.log("Servidor corriendo en puerto 3010");
 });
 app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views/index.html"));
+    res.sendFile(path.join(__dirname, "views/index.html"));
 });
 app.get("/login", (req, res) => {
-  res.sendFile(path.join(__dirname, "views/login.html"));
+    res.sendFile(path.join(__dirname, "views/login.html"));
 });
 app.get("/register", (req, res) => {
-  res.sendFile(path.join(__dirname, "views/register.html"));
+    res.sendFile(path.join(__dirname, "views/register.html"));
 });
 app.get("/nav", (req, res) => {
-  res.sendFile(path.join(__dirname, "views/nav.html"));
+    res.sendFile(path.join(__dirname, "views/nav.html"));
 });
 app.get("/cart", (req, res) => {
-  res.sendFile(path.join(__dirname, "views/compras.html"));
+    res.sendFile(path.join(__dirname, "views/compras.html"));
 });
 app.get("/product", (req, res) => {
-  res.sendFile(path.join(__dirname, "views/producto.html"));
+    res.sendFile(path.join(__dirname, "views/product.html"));
 });
