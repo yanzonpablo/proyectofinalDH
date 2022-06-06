d014226e782ae4d12842ecf368be352104e2d8ee
EnriqueAlfonso <enrique.alfonso.lema@gmail.com>
2022-06-06 19:04:14 -0300

    19:04

--- desarrollo/app.js
+++ desarrollo/app.js
@@ -3,16 +3,20 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+/*
 <<<<<<< HEAD
 =======
 const mainRouter = require("./routes/main-routes");
 app.use(express.static(path.join(__dirname, "public")));
 
->>>>>>> c320fa157fbb5fdeb362f9aa6aa0523e1c5cac50
+>>>>>>> 
+*/
 app.listen(3010, () => {
     console.log("Servidor corriendo en puerto 3010");
 });
+/*
 <<<<<<< HEAD
+*/
 app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "views/index.html"));
@@ -32,8 +36,9 @@ app.get("/cart", (req, res) => {
 app.get("/product", (req, res) => {
     res.sendFile(path.join(__dirname, "views/product.html"));
 });
-=======
 
 app.set("view engine", "ejs");
 app.use("/", mainRouter);
+/*
 >>>>>>> c320fa157fbb5fdeb362f9aa6aa0523e1c5cac50
+*/
