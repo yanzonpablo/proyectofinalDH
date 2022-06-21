72cc68ad33e3de2e541d94c827aecb666c2f2823
nicoquartero <nicoquartero@gmail.com>
2022-06-20 21:47:21 -0300

    Implementacion de creacion de producto

--- desarrollo/controllers/main-controller.js
+++ desarrollo/controllers/main-controller.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const express = require("express");
 const products = require("../data/db-products");
+const productsFilePath = path.join(__dirname, "../data/products.json");
 const allProducts = products.getAll();
 
 module.exports = {
--- desarrollo/controllers/products-controller.js
+++ desarrollo/controllers/products-controller.js
@@ -1,19 +1,34 @@
 const path = require("path");
+const fs = require("fs");
 const express = require("express");
-const db = require("../data/db-products");
-const allProducts = db.getAll();
+const products = require("../data/db-products");
+const productsFilePath = path.join(__dirname, "../data/products.json");
+const allProducts = products.getAll();
 
 module.exports = {
   create: (req, res) => {
     res.render("cargarProducto");
   },
   details: (req, res) => {
+    // RESTA HACER DINAMICO EL PRODUCT.HTML
     res.render("product");
+    // Comentario para renderizar producto en base a su ID
+    /* res.render("product", {
+      product: allProducts.find(p) => p.id == req.params.id
+    })*/
   },
-  // RESTA HACER DINAMICO EL PRODUCT.HTML
   store: (req, res) => {
-    res.send(req.body);
-    // res.redirect("/");
+    const newProduct = req.body;
+    // Crear id en base al ultimo
+    if (allProducts.length) {
+      newProduct.id = allProducts[allProducts.length - 1].id + 1;
+    } else {
+      newProduct.id = 1;
+    }
+    allProducts.push(newProduct);
+    const fileTxt = JSON.stringify(allProducts, null, 4);
+    fs.writeFileSync(productsFilePath, fileTxt);
+    res.redirect("/");
   },
   edit: (req, res) => {
     res.render("editarProducto");
