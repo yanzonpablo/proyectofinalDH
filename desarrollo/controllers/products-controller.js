5edd1d43d7745e13d90d499a51d657dffd0100fc
nicoquartero <nicoquartero@gmail.com>
2022-06-22 19:41:08 -0300

    Tipos de archivos permitidos en formularios de carga/edicion productos

--- desarrollo/controllers/products-controller.js
+++ desarrollo/controllers/products-controller.js
@@ -42,11 +42,10 @@ module.exports = {
       allProducts[allProducts.findIndex((p) => p.id == req.params.id)];
 
     if (req.file) {
-      const pathAbsolute = path.join(__dirname, "../../public", product.imagen);
+      const pathAbsolute = path.join(__dirname, "../public", product.imagen);
       fs.unlinkSync(pathAbsolute);
       product.imagen = req.file.filename;
     }
-
     product.nombre = req.body.nombre;
     product.precio = req.body.precio;
     product.descripcion = req.body.descripcion;
