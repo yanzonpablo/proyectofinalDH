0289393fbdd43b6b4259aab23801b69326b11140
Enrique Alfonso <enrique.alfonso.lema@gmail.com>
2022-06-26 15:13:08 -0300

    15:12

--- desarrollo/app.js
+++ desarrollo/app.js
@@ -1,18 +1,22 @@
+/* Modulos Nativos. */
 const fs = require("fs");
-const express = require("express");
 const path = require("path");
-const methodOverride = require("method-override");
-const app = express();
 
+/* Modulos Instalados. */
+const express = require("express");
+const app = express();
+const methodOverride = require("method-override");
+
+/* Modulos Personales. */
 const mainRouter = require("./routes/main-routes");
 
-app.use(express.static(path.join(__dirname, "public"))); //Uso de archivo de carpeta 'public'
-app.use(express.urlencoded({ extended: false })); //Obtencion de datos de form
-
 app.listen(3010, () => {
-  console.log("Servidor corriendo en puerto 3010");
+    console.log("Port: 3010");
 });
 
 app.set("view engine", "ejs"); //Template engine
-app.use(methodOverride("_method")); //Uso de PUT and DELETE
+
 app.use("/", mainRouter); //Routeo principal
+app.use(express.static(path.join(__dirname, "public"))); //Uso de archivo de carpeta 'public'
+app.use(express.urlencoded({ extended: false })); //Obtencion de datos de form
+app.use(methodOverride("_method")); //Uso de PUT and DELETE
