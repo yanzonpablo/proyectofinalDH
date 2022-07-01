d85fc3e5a31b014606597d2e689c06445d1d3754
nicoquartero <nicoquartero@gmail.com>
2022-07-01 20:22:09 -0300

    User router en main-routes

--- desarrollo/app.js
+++ desarrollo/app.js
@@ -7,19 +7,17 @@ const express = require("express");
 const app = express();
 const methodOverride = require("method-override");
 
-/* Modulos Personales. */
+/* Ruteo principal */
 const mainRouter = require("./routes/main-routes");
-const userRoutes = require("./routes/users-routes");
 
 // Conexion al puerto 3010 "localhost"
 app.listen(3010, () => {
-    console.log("Port: 3010");
+  console.log("Port: 3010");
 });
 
 app.set("view engine", "ejs"); // Template engine
 
 app.use("/", mainRouter); // Routeo principal
-app.use("/user", userRoutes); // Ruteo usuarios
 
 app.use(express.static(path.join(__dirname, "public"))); // Uso de archivo de carpeta 'public'
 app.use(express.urlencoded({ extended: false })); // Obtencion de datos de form
@@ -27,5 +25,5 @@ app.use(methodOverride("_method")); // Uso de PUT and DELETE
 
 // Implementacion Error 404
 app.use((req, res, next) => {
-    res.status(404).render("not-found");
+  res.status(404).render("not-found");
 });
