9523acffee922a1e165429873dab24c7eb14333b
nicoquartero <nicoquartero@gmail.com>
2022-07-16 19:58:24 -0300

    Registro usuario - con error de creacion

--- desarrollo/controllers/auth-controller.js
+++ desarrollo/controllers/auth-controller.js
@@ -10,37 +10,44 @@ module.exports = {
         res.render("login");
     },
     login: (req, res) => {
-        const { email, password } = req.body;
-        const user = usersDB.findByEmail(email);
-        if (user && (password == user.password)) {
-            req.session.loggedUser = user;
-            
-            res.redirect("/");
-            return;
-        }
-        res.render("login", {
-            error: true,
-        });
-    },
-    register: (req, res) => {
         const resultValidation = validationResult(req);
         if (!resultValidation.isEmpty()) {
-            return res.render("register", {
+            return res.render("login", {
                 errors: resultValidation.mapped(),
             });
         }
-
-        // const newUser = {
-        //     ...req.body,
-        //     id: usersDB.getNewId(),
-        //     password: bcrypt.hashSync(req.body.password, 10),
-        // };
-        // delete newUser.rePassword;
-
-        // const users = usersDB.getAll();
-        // users.push(newUser);
-        // usersDB.saveAll(users);
-
-        // res.redirect("/login");
+        else {
+            const { email, password } = req.body;
+            const user = usersDB.findByEmail(email);
+            if (user && (password == user.password)) {
+                req.session.loggedUser = user;
+    
+                res.redirect("/");
+                return;
+            }
+            res.render("login", {
+                error: true,
+            });
+        }
+    },
+    register: (req, res) => {
+/*         
+        const resultValidation = validationResult(req);
+        if (!resultValidation.isEmpty()) {
+             res.render("register", {
+                errors: resultValidation.mapped(),
+            });
+            return;
+        } */
+        const newUser = {
+            ...req.body,
+            id: usersDB.getNewId(),
+            // password: bcrypt.hashSync(req.body.password, 10),
+        };
+        delete newUser.rePassword;
+        const users = usersDB.getAll();
+        users.push(newUser);
+        usersDB.saveAll(users);
+        res.redirect("/login");
     },
 };
--- desarrollo/routes/auth-router.js
+++ desarrollo/routes/auth-router.js
@@ -2,12 +2,13 @@ const express = require("express");
 
 const authController = require("../controllers/auth-controller");
 const registerValidation = require("../validations/register-validations");
+const loginValidation = require("../validations/login-validations");
 
 const router = express.Router();
 
 router.get("/login", authController.showLogin);
 router.post("/login", authController.login);
-router.get("/register", authController.showRegister);
+router.get("/register", loginValidation, authController.showRegister);
 router.post("/register", registerValidation, authController.register);
 
 module.exports = router;
