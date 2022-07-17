71b26a7c083acd202c3aae1853b005a269acb842
nicoquartero <nicoquartero@gmail.com>
2022-07-17 16:06:44 -0300

    Multer en form de registro de usuario

--- desarrollo/routes/auth-router.js
+++ desarrollo/routes/auth-router.js
@@ -2,13 +2,15 @@ const express = require("express");
 
 const authController = require("../controllers/auth-controller");
 const registerValidation = require("../validations/register-validations");
-const loginValidation = require("../validations/login-validations");
+
+const upload = require("../middlewares/multer");
+
 
 const router = express.Router();
 
 router.get("/login", authController.showLogin);
 router.post("/login", authController.login);
-router.get("/register", loginValidation, authController.showRegister);
-router.post("/register", registerValidation, authController.register);
+router.get("/register", authController.showRegister);
+router.post("/register", registerValidation, upload.single('imagen'), authController.register);
 
 module.exports = router;
