886ea04c663935c286ec7619303c2f03e6b2d22f
nicoquartero <nicoquartero@gmail.com>
2022-07-17 19:03:37 -0300

    Validacion para mail duplicado

--- desarrollo/controllers/auth-controller.js
+++ desarrollo/controllers/auth-controller.js
@@ -12,27 +12,42 @@ module.exports = {
     login: (req, res) => {
         const { email, password } = req.body;
         const user = usersDB.findByEmail(email);
-        if (user && (password == user.password)) {
+            if (user && (password == user.password)) {
             req.session.loggedUser = user;
+    
             res.redirect("/");
             return;
-        }
+            }
             res.render("login", {
                 error: true,
-        });
+            });
     },
     register: (req, res) => {
         const resultValidation = validationResult(req);
         if (!resultValidation.isEmpty()) {
              res.render("register", {
                 errors: resultValidation.mapped(),
+                oldData: req.body,
             });
             return;
         }
+        const userExists = usersDB.findByEmail(req.body.email)
+
+        if (userExists) {
+            return res.render("register", {
+                errors: {
+                    email: {
+                        msg: 'Ya existe una cuenta vinculada a ese email'
+                    }
+                }
+            })
+        }
+
         const newUser = {
             ...req.body,
             id: usersDB.getNewId(),
             password: bcrypt.hashSync(req.body.password, 10),
+            imagen: req.file.filename
         };
         delete newUser.rePassword;
         const users = usersDB.getAll();
