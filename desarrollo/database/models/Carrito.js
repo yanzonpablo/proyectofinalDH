f40ffd57a51b30bd5d2427549d53045873b92b57
yanzonpablo <yanzonpablo@gmail.com>
2022-08-03 21:15:33 -0300

    creacion modelo carrito

---
+++ desarrollo/database/models/Carrito.js
@@ -0,0 +1,31 @@
+module.exports = (sequelize, dataTypes) => {
+  let alias = "Carritos";
+  let cols = {
+    id: {
+      type: dataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    totalCantidad: {
+      type: dataTypes.INTEGER,
+      allowNull: false,
+    },
+    totalPrecio: {
+      type: dataTypes.INTEGER,
+      allowNull: false,
+    }
+  };
+
+      let config = {
+        tableName: "carrito",
+        timestamps: true,
+        createdAt: "created_at",
+        updatedAt: "updated_at",
+        deletedAt: false,
+      };
+
+    const Carrito = sequelize.define(alias, cols, config);
+
+    return Carrito;
+  }
