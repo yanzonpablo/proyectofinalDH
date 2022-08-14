23e40d8664f6eb7a9f213e29a7192ed3a8aca700
nicoquartero <nicoquartero@gmail.com>
2022-08-14 17:42:09 -0300

    Creacion de CRUD products sin asociaciones

--- desarrollo/controllers/products-controller.js
+++ desarrollo/controllers/products-controller.js
@@ -13,60 +13,40 @@ module.exports = {
       res.render("product", { products });
     });
   },
-
-  edit: (req, res) => {
-    db.Products.findByPk(req.params.id).then((product) => {
-      res.render("product", { producto: product });
-    });
-  },
-  /*
-  update: (req, res) => {
-    const product =
-      allProducts[allProducts.findIndex((p) => p.id == req.params.id)];
-
-    product.nombre = req.body.nombre;
-    product.precio = req.body.precio;
-    product.descripcion = req.body.descripcion;
-    product.categoria = req.body.categoria;
-    product.seccion = req.body.seccion;
-    product.descuento = req.body.descuento;
-
-    if (req.file) {
-      const pathAbsolute = path.join(__dirname, "../public", product.imagen);
-      fs.unlinkSync(pathAbsolute);
-      product.imagen = req.file.filename;
-    }
-
-    productsFS.saveAll(allProducts);
-    res.redirect("/products");
-  },
   create: (req, res) => {
     res.render("cargarProducto", { categories: allCategories });
   },
+  edit: (req, res) => {
+    db.Products.findByPk(req.params.id).then((product) => {
+      res.render("editarProducto", { productToEdit: product });
+    });
+  },
   store: (req, res) => {
-    const newProduct = req.body;
-    // Crear id en base al ultimo
-    if (allProducts.length) {
-      newProduct.id = allProducts[allProducts.length - 1].id + 1;
-    } else {
-      newProduct.id = 1;
-    }
-    if (req.file) {
-      newProduct.imagen = req.file.filename;
-    } else {
-      newProduct.imagen = "prod-default.jpg";
-    }
-
-    allProducts.push(newProduct);
-    productsFS.saveAll(allProducts);
-    res.redirect("/products");
+    db.Products.create({
+      ...req.body,
+    }).then(() => {
+      res.redirect("/products");
+    });
+  },
+  update: (req, res) => {
+    db.Products.update(
+      {
+        ...req.body,
+      },
+      {
+        where: { id: req.params.id },
+      }
+    ).then(() => {
+      res.redirect("/products");
+    });
   },
   destroy: (req, res) => {
-    const filteredProducts = allProducts.filter((p) => {
-      return p.id != req.params.id;
+    db.Products.destroy({
+      where: {
+        id: req.params.id,
+      },
+    }).then(() => {
+      res.redirect("/products");
     });
-    productsFS.saveAll(filteredProducts);
-    res.redirect("/products");
   },
-  END TESTING */
 };
--- desarrollo/routes/products-routes.js
+++ desarrollo/routes/products-routes.js
@@ -8,15 +8,7 @@ const validateProductId = require("../middlewares/validate-product-id");
 
 //LIST
 router.get("/", productsController.index); //Listado de productos
-//EDIT
-router.get("/edit/:id", productsController.edit); //Vista de edicion de producto
-/*
-router.put(
-  "/:id",
-  uploadProduct.single("imagen_producto"),
-  productsController.update
-); //PUT de edicion de producto
-/* TESTING
+
 //CREATE
 router.get("/create/", productsController.create); //Vista formulario de creacion
 router.post(
@@ -24,6 +16,16 @@ router.post(
   uploadProduct.single("imagen_producto"),
   productsController.store
 ); //POST de creacion de producto
+
+//EDIT
+router.get("/edit/:id", productsController.edit); //Vista de edicion de producto
+router.put(
+  "/:id",
+  uploadProduct.single("imagen_producto"),
+  productsController.update
+); //PUT de edicion de producto
+
+/* TESTING
 //DELETE
 router.delete("/:id", productsController.destroy); //DELETE de producto
 */
