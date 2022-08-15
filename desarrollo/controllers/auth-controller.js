23beb407818101db2ac0ac20b0d8ac31650c8e1a
nicoquartero <nicoquartero@gmail.com>
2022-08-15 17:21:18 -0300

    Login y registro completo

--- desarrollo/controllers/auth-controller.js
+++ desarrollo/controllers/auth-controller.js
@@ -1,71 +1,75 @@
 const bcrypt = require("bcryptjs");
 const { validationResult } = require("express-validator");
-const usersDB = require("../models/db-users");
+// const usersDB = require("../models/db-users");
+const db = require("../database/models");
 
 module.exports = {
-    showRegister: (req, res) => {
-        res.render("register");
-    },
-    showLogin: (req, res) => {
-        res.render("login");
-    },
-    login: (req, res) => {
-        const { email, password } = req.body;
-        const user = usersDB.findByEmail(email);
-            if (user && bcrypt.compareSync(password, user.password)) {
-            delete user.password; // Se borra la password por seguridad
-            req.session.loggedUser = user;
-
-            if (req.body.recordame == 'on') {
-                res.cookie('userEmail', req.body.email, { maxAge: 10000})
-            }
-    
-            res.redirect("/");
-            return;
-            }
-            res.render("login", {
-                error: true,
-            });
-    },
-    register: (req, res) => {
-        const resultValidation = validationResult(req);
-        if (!resultValidation.isEmpty()) {
-             res.render("register", {
-                errors: resultValidation.mapped(),
-                oldData: req.body,
-            });
-            return;
+  showRegister: (req, res) => {
+    res.render("register");
+  },
+  showLogin: (req, res) => {
+    res.render("login");
+  },
+  login: (req, res) => {
+    const { email, password } = req.body;
+    db.Users.findOne({ where: { email: req.body.email } }).then((user) => {
+      if (user && password == user.password) {
+        req.session.loggedUser = user;
+        if (req.body.recordame == "on") {
+          res.cookie("userEmail", req.body.email, { maxAge: 10000 });
         }
-        const userExists = usersDB.findByEmail(req.body.email)
-        if (userExists) {
-            return res.render("register", {
-                errors: {
-                    email: {
-                        msg: 'Ya existe una cuenta vinculada a ese email'
-                    }
-                },
-                oldData: req.body,
-            })
-        }
-        const newUser = {
-        ...req.body,
-        id: usersDB.getNewId(),
-        password: bcrypt.hashSync(req.body.password, 10),
-        };
-        if (req.file) {
-            newUser.imagen = req.file.filename;
-        } else {
-            newUser.imagen = 'user-default.jpg'
-        };
-        delete newUser.rePassword;
-        const users = usersDB.getAll();
-        users.push(newUser);
-        usersDB.saveAll(users);
-        res.redirect("/login");
-    },
-    logout: (req, res) => {
-        res.clearCookie('userEmail');
-        req.session.destroy();
-        res.redirect('/');
+        res.redirect("/");
+        return;
+      }
+      res.render("login", {
+        error: true,
+      });
+    });
+  },
+  register: (req, res) => {
+    const resultValidation = validationResult(req);
+    if (!resultValidation.isEmpty()) {
+      res.render("register", {
+        errors: resultValidation.mapped(),
+        oldData: req.body,
+      });
+      return;
     }
+    db.Users.findOne({ where: { email: req.body.email } })
+      .then((userExists) => {
+        if (userExists) {
+          return res.render("register", {
+            errors: {
+              email: {
+                msg: "Ya existe una cuenta vinculada a ese email",
+              },
+            },
+            oldData: req.body,
+          });
+        }
+      })
+      .then(() => {
+        db.Users.create({
+          ...req.body,
+        }).then(() => {
+          res.redirect("/login");
+        });
+      });
+    /* 
+    if (req.file) {
+      newUser.imagen = req.file.filename;
+    } else {
+      newUser.imagen = "user-default.jpg";
+    }
+    delete newUser.rePassword;
+    const users = usersDB.getAll();
+    users.push(newUser);
+    usersDB.saveAll(users);
+    */
+  },
+  logout: (req, res) => {
+    res.clearCookie("userEmail");
+    req.session.destroy();
+    res.redirect("/");
+  },
 };
--- desarrollo/validations/register-validations.js
+++ desarrollo/validations/register-validations.js
@@ -1,23 +1,26 @@
 const { body } = require("express-validator");
 
 module.exports = [
-    body("nombre_apellido").notEmpty().withMessage("El campo no puede estar vacío"),
-    body("email").isEmail().withMessage("Debe introducir un email valido"),
-    body("password").isLength({min: 8}).withMessage("La contraseña debe tener al menos 8 caracteres"),
-    body("rePassword").custom((valor, extra) => {
-        if (valor == undefined) {
-            throw new Error("Debe confirmar su contraseña")
-        }
-        if (valor !== extra.req.body.password) {
-            throw new Error("Las contraseñas no coinciden");
-        }
-        return true;
-    }),
-    body("terms").custom((value) => {
-        if (value != undefined) {
-            return true;
-        } else {
-            throw new Error("Debe aceptar los términos y condiciones")
-        }
-    }),
-]
+  body("nombre").notEmpty().withMessage("El campo no puede estar vacío"),
+  body("apellido").notEmpty().withMessage("El campo no puede estar vacío"),
+  body("email").isEmail().withMessage("Debe introducir un email valido"),
+  body("password")
+    .isLength({ min: 8 })
+    .withMessage("La contraseña debe tener al menos 8 caracteres"),
+  body("rePassword").custom((valor, extra) => {
+    if (valor == undefined) {
+      throw new Error("Debe confirmar su contraseña");
+    }
+    if (valor !== extra.req.body.password) {
+      throw new Error("Las contraseñas no coinciden");
+    }
+    return true;
+  }),
+  body("terms").custom((value) => {
+    if (value != undefined) {
+      return true;
+    } else {
+      throw new Error("Debe aceptar los términos y condiciones");
+    }
+  }),
+];
