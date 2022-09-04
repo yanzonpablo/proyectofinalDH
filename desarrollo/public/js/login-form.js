c3b33d4f5545928ff29855b5ea00bd994dce7236
Michele Mazzinghi <mmazz91@gmail.com>
2022-09-04 14:06:14 -0300

    Validaciones frontend formularios login y register

--- desarrollo/public/js/login-form.js
+++ desarrollo/public/js/login-form.js
@@ -1,6 +1,4 @@
 const form = document.querySelector("form");
-const nombre = document.querySelector("#nombre");
-const apellido = document.querySelector("#apellido");
 const email = document.querySelector("#email");
 const password = document.querySelector("#password");
 const errores = document.querySelector("#errores");
@@ -11,26 +9,10 @@ window.addEventListener("load", function () {
     e.preventDefault();
     mensajesErrores = [];
 
-    if (nombre.value == "") {
-      mensajesErrores = mensajesErrores.concat("El nombre debe estar completo");
-    } else if (nombre.value.length < 4) {
-      mensajesErrores = mensajesErrores.concat(
-        "El nombre debe tener más de tres caracteres"
-      );
-    }
-
-    if (apellido.value == "") {
-      mensajesErrores = mensajesErrores.concat(
-        "El apellido debe estar completo"
-      );
-    } else if (apellido.value.length < 4) {
-      mensajesErrores = mensajesErrores.concat(
-        "El apellido debe tener más de tres caracteres"
-      );
-    }
-
     if (email.value == "") {
-      mensajesErrores = mensajesErrores.concat("Ingresa tu email");
+      mensajesErrores = mensajesErrores.concat(
+        "Ingresa tu email"
+      );
     }
 
     if (password.value == "") {
@@ -38,7 +20,7 @@ window.addEventListener("load", function () {
     }
 
     if (mensajesErrores.length > 0) {
-      let ulErrores = document.querySelector(".errores");
+      let ulErrores = document.querySelector("ul");
       for (let i = 0; i < mensajesErrores.length; i++) {
         ulErrores.innerHTML += "<li>" + mensajesErrores[i] + "</li>";
       }
