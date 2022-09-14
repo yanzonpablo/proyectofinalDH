92ad8e297024ea562de9b56906b6a6be95ed3831
nicoquartero <nicoquartero@gmail.com>
2022-09-14 19:17:27 -0300

    Creacion de estructura de dashboard trabajado en clase

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import BigCard from "./components/BigCard";
+import MiniCard from "./components/MiniCard";
+import Sidebar from "./components/Sidebar";
+
+const miniCards = [
+  {
+    id: "5",
+    title: "Products in Database",
+    value: "35",
+    icon: "fa-film",
+  },
+  {
+    id: "24",
+    title: "Users in Database",
+    color: "success",
+    value: "10",
+    icon: "fa-award",
+  },
+  {
+    id: "32",
+    title: "Categories in Database",
+    color: "warning",
+    value: "6",
+    icon: "fa-user",
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="wrapper">
+      {/* <!-- Sidebar --> */}
+      <Sidebar />
+      {/* <!-- End of Sidebar --> */}
+
+      {/* <!-- Content Wrapper --> */}
+      <div id="content-wrapper" className="d-flex flex-column">
+        {/* <!-- Main Content --> */}
+        <div id="content">
+          {/* <!-- Content Row Top --> */}
+          <div className="container-fluid pt-5">
+            <div className="d-sm-flex align-items-center justify-content-between mb-4">
+              <h1 className="h3 mb-0 text-gray-800">App Dashboard</h1>
+            </div>
+
+            {/* <!-- Content Row Movies--> */}
+            <div className="row">
+              {/* <!-- Movies in Data Base --> */}
+              {miniCards.map((data) => {
+                return <MiniCard {...data} key={data.id} />;
+              })}
+            </div>
+            {/* <!-- End movies in Data Base --> */}
+
+            {/* <!-- Content Row Last Movie in Data Base --> */}
+            <div className="row">
+              {/* <!-- Last Movie in DB --> */}
+              <BigCard />
+              {/* <!-- End content row last movie in Data Base --> */}
+
+              {/* <!-- Genres in DB --> */}
+              <BigCard />
+            </div>
+          </div>
+          {/* <!--End Content Row Top--> */}
+        </div>
+        {/* <!-- End of MainContent --> */}
+
+        {/* <!-- Footer --> */}
+        <footer className="sticky-footer bg-white">
+          <div className="container my-auto">
+            <div className="copyright text-center my-auto">
+              <span>Copyright &copy; Dashboard 2021</span>
+            </div>
+          </div>
+        </footer>
+        {/* <!-- End of Footer --> */}
+      </div>
+      {/* <!-- End of Content Wrapper --> */}
     </div>
   );
 }
