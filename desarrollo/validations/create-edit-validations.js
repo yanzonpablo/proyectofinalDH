010692df76017ea4a4b4865d94e17b551d923fa4
nicoquartero <nicoquartero@gmail.com>
2022-09-15 16:42:27 -0300

    Validacion de imagen backend faltante

--- desarrollo/validations/create-edit-validations.js
+++ desarrollo/validations/create-edit-validations.js
@@ -4,5 +4,14 @@ module.exports = [
     body("nombre").isLength({ min: 5 }).withMessage("* El nombre debe tener al menos cinco caracteres"),
     body("descripcion").isLength({ min: 20 }).withMessage("* La descripción debe tener al menos veinte carácteres"),
     body("precio").custom(value=>{ if((value <= 0) || (value == "0")){throw new Error ("* El precio no puede ser 0 o negativo")}else{return true}}),
-    body("descuento").custom(value=>{if((value >= 100) || (value <0)){throw new Error ("* El descuento no puede ser del 100% o mayor")}else{return true}})
+    body("descuento").custom(value=>{if((value >= 100) || (value <0)){throw new Error ("* El descuento no puede ser del 100% o mayor")}else{return true}}),
+    body("imagen").custom((element,{req}) => {
+      const validImg = [".png",".jpg",".jpeg",".gif"]
+      if (req.file) {
+        const fileExt = req.file.mimetype;
+        if(!validImg.includes(fileExt)) {
+          throw new Error("* La imagén solo puede ser de las siguientes extensiones '.png','.jpg','.jpeg','.gif'");
+        }
+      }
+    })
   ];
--- desarrollo/validations/edit-category.js
+++ desarrollo/validations/edit-category.js
@@ -3,4 +3,13 @@ const { body } = require("express-validator");
 module.exports = [
     body("nombre").isLength({ min: 3 }).withMessage("* El nombre debe contener un mínimo tres caracteres"),
     body("descripcion").isLength({ min: 3 }).withMessage("* La descripción debe contener al menos tres carácteres"),
+    body("imagen").custom((element,{req}) => {
+      const validImg = [".png",".jpg",".jpeg",".gif"]
+      if (req.file) {
+        const fileExt = req.file.mimetype;
+        if(!validImg.includes(fileExt)) {
+          throw new Error("* La imagén solo puede ser de las siguientes extensiones '.png','.jpg','.jpeg','.gif'");
+        }
+      }
+    })
   ];
