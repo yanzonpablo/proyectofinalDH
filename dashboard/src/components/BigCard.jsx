ad2f08cbd184970d325c1ac2ec8ddfc4891b5ced
Pablo Yanzón <70301570+yanzonpablo@users.noreply.github.com>
2023-01-08 19:29:45 -0300

    Update BigCard.jsx

--- dashboard/src/components/BigCard.jsx
+++ dashboard/src/components/BigCard.jsx
@@ -3,7 +3,7 @@ export default function BigCard({ title, children }) {
       <div className="col-lg-6 mb-4">
           <div className="card shadow mb-4">
               <div className="card-header py-3">
-                  <h5 className="m-0 font-weight-bold text-gray-800">
+                  <h5 className="m-0 font-weight-bold text-gray-800"> 
                       {title}
                   </h5>
               </div>
